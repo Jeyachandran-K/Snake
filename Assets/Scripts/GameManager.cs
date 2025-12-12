4d95fac8032d16bbda2e34d6aee397c017d5a4de
Jeyachandran <jeyachandran.work@gmail.com>
2025-12-12 14:31:25 +0530

    GameManager Class completed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,19 +7,25 @@ public class GameManager : MonoBehaviour
     private int gridWidth = 20;
     private int gridHeight = 20;
     private Vector2Int foodPadding = new Vector2Int(1, 1);
+    private Vector2Int foodPosition;
+
+    [SerializeField] private Snake snake;
+    [SerializeField] private Food food;
 
     [Header("Prefabs")]
-    [SerializeField] private Snake snake;
-    //[SerializeField] private Food food;
-
-    [Header("GamePlay")]
-    [SerializeField] private float moveInterval = 0.15f;
-
+    [SerializeField] private GameObject foodPrefab;
+    [SerializeField] private GameObject snakeBodyPrefab;
     private bool isRunning = false;
 
     private void Awake()
     {
         Instance = this;
+        
+    }
+
+    private void Start()
+    {
+        SpawnFood();
     }
 
     private Vector2Int GetRandomEmptyGridPosition()
@@ -44,10 +50,24 @@ public class GameManager : MonoBehaviour
     }
     private Vector2 WorldToGrid(Vector3 worldPosition)
     {
-        float offSetX = gridWidth / 2 + 0.5f;
-        float offSetY = gridHeight/ 2 + 0.5f;
+        float offSetX =Mathf.FloorToInt( gridWidth / 2 + 0.5f);
+        float offSetY =Mathf.FloorToInt( gridHeight/ 2 + 0.5f);
 
         return new Vector2(worldPosition.x+offSetX,worldPosition.y +offSetY);
     }
 
+    private void SpawnFood()
+    {
+        foodPosition=GetRandomEmptyGridPosition();
+        Vector3 worldFoodPosition = GridToWorld(foodPosition);
+        Instantiate(foodPrefab, worldFoodPosition, Quaternion.identity);
+    }
+    public void OnFoodEaten()
+    {
+        SpawnFood();
+    }
+    public bool IsInsideGrid(Vector2Int gridPosition)
+    {
+        return gridPosition.x>0 && gridPosition.y >0 && gridPosition.x<gridWidth && gridPosition.y<gridHeight;
+    }
 }
